0ec5a07f2984a0c009e081ac1189199de41901be
duahuss <duahussainn@gmail.com>
2024-11-28 12:29:34 -0500

    a

---
+++ e11-mal/pages/csc309-e11.js
@@ -0,0 +1,9 @@
+export default function AutoTester() {
+    return (
+      <div id="auto-tester">
+        Hi. I am rahma987, and I have learned how to develop a full-stack web app
+        and deploy it!
+      </div>
+    );
+  }
+  
